de40213f538fc753d7c3c556a90b3f2951f2a3ec
Shubhamproperty <vy458367@gmail.com>
2026-08-05 11:08:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,42 +8,47 @@ const supabaseClient = window.supabase.createClient(
     SUPABASE_KEY
 );
 
-alert("CONNECTED SUCCESSFULLY");
+
 
 async function loadproperties() {
-
-    alert("LOADING PROPERTIES");
 
     const { data, error } = await supabaseClient
         .from("properties")
         .select("*");
-
-    alert("AFTER SELECT");
 
     if (error) {
         alert("ERROR = " + error.message);
         return;
     }
 
-    alert("TOTAL ROWS = " + data.length);
 
-    const container = document.getElementById("properties-container");
 
-    alert("CONTAINER = " + container);
-
+    const container =
+        document.getElementById("properties-container");
     container.innerHTML = "";
-
-    alert("FOR EACH START");
-
     data.forEach((property) => {
-        alert("PHOTO URL = " = +property.image_urls);
 
-        alert("TITLE = " + property.title);
+        let imageUrl = "";
+
+        if
+            (property.image_urls.startsWith("[")) {
+
+            imageUrl = JSON.parse(property.image_urls)[0];
+
+        } else {
+            imageUrl = property.image_urls;
+        }
+
 
         container.innerHTML += `
         <div>
 
-            <img src="${property.image_urls}" width="300">
+          <img src="${imageUrl}"
+          width="300">
+          
+
+
+           
 
             <h2>${property.title}</h2>
 
